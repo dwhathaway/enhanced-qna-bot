4029c1182e06e6b2f6576ad6293732cd44e4a585
David Hathaway <dahat@microsoft.com>
2017-12-26 14:32:59 -0800

    Removed unnecessary build/deploy/publish scripts that are no longer valid.

--- src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=pot-bot-deploy-Web-Deploy.pubxml -p:Password=2niLpaLyw9xrj0haahR34qjk7ek9ytyNs5KBaNxdHiKsruEizLr1ndBurqaZ
-
